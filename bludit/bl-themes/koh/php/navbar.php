9ac529017396222aa54b2d316eda4758f8e6b8b4
kawww <39939137+kawww@users.noreply.github.com>
2020-12-28 08:24:26 +0800

    Add files via upload

--- bludit/bl-themes/koh/php/navbar.php
+++ bludit/bl-themes/koh/php/navbar.php
@@ -1,6 +1,16 @@
+<?php
+
+if(!$_REQ["asset"] or $_REQ["sort"]==0){$sort=1;}
+if($_REQ["sort"]==1){$sort=0;asort($listasset);}
+
+if(!$theme){$theme=$_REQ["theme"];}
+
+$navlink="?lang=&asset=".$_REQ["asset"]."&theme=".$theme."&scode=".$_REQ["scode"]."&group=no&gname=".$_REQ["gname"]."&sort=".$sort;
+?>
+
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark text-uppercase">
 	<div class="container">
-		<a class="navbar-brand" href="<?php echo Theme::siteUrl(); ?>">
+		<a class="navbar-brand" href=<?php echo $navlink; ?>>
 			<span class="text-white"><font color=#28f428><?php echo $site->title(); ?></font></span>
 		</a><?php include("theme.php"); ?>
 		
@@ -11,4 +21,3 @@
 		</div>
 	</div>
 </nav>
-<br><br>
