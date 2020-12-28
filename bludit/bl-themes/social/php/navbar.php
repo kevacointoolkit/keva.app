304d791d6cb2e73d6cc16f882d42e187502577f3
kawww <39939137+kawww@users.noreply.github.com>
2020-12-28 08:19:35 +0800

    Add files via upload

--- bludit/bl-themes/social/php/navbar.php
+++ bludit/bl-themes/social/php/navbar.php
@@ -1,6 +1,18 @@
+<?php
+
+if(!$_REQ["asset"] or $_REQ["sort"]==0){$sort=1;}
+if($_REQ["sort"]==1){$sort=0;asort($listasset);}
+
+if(!$theme){$theme=$_REQ["theme"];}
+
+if(!$_REQ["theme"]){$theme="social";}
+
+$navlink="?lang=&asset=".$_REQ["asset"]."&theme=".$theme."&scode=".$_REQ["scode"]."&group=no&gname=".$_REQ["gname"]."&sort=".$sort;
+?>
+
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark text-uppercase">
 	<div class="container">
-		<a class="navbar-brand" href="/">
+		<a class="navbar-brand" href=<?php echo $navlink; ?>>
 			<span class="text-white"><font color=#28f428><?php echo $site->title(); ?></font></span>
 		</a><?php include("theme.php"); ?>
 		
