8e7a79523635d5b5a322249fcfdedd5b20572959
kawww <39939137+kawww@users.noreply.github.com>
2021-01-31 00:07:35 +0800

    Add files via upload

--- bludit/cyber/tia.php
+++ bludit/cyber/tia.php
@@ -456,7 +456,7 @@ var items=[<?php if(is_numeric($tia)==true)
 
 				
 
-				If($key=="_KEVA_NS_" or $key=="SYSWORDS" or $key=="TIA" or $key=="ANN" or substr($value,0,12)=="mimblewimble"){continue;}
+				If($key=="_KEVA_NS_" or $key=="\u0001_KEVA_NS_" or $key=="SYSWORDS" or $key=="TIA" or $key=="ANN" or substr($value,0,12)=="mimblewimble"){continue;}
 
 				
 				$key=str_replace("'", '&#8217;', $key);
