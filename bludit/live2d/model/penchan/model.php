5d051caa47e4cef0c3c8768811a7c6a1f99dd29d
kawww <39939137+kawww@users.noreply.github.com>
2021-02-01 09:35:21 +0800

    Add files via upload

---
+++ bludit/live2d/model/penchan/model.php
@@ -0,0 +1,13 @@
+ {
+    "type": "Live2D Model Setting",
+	"name": "penchan",
+	"model": "penchan.moc",
+<?php
+	
+	$prand=$_REQUEST["model"];
+	
+    echo "\"textures\":[\"penchan/".$prand.".png\"]";
+    
+?>
+
+}
