dffe06f2e191028b120a3c061e8c41ca36c058f3
kawww <39939137+kawww@users.noreply.github.com>
2021-02-02 07:17:07 +0800

    Add files via upload

--- bludit/bl-themes/roam/php/home.php
+++ bludit/bl-themes/roam/php/home.php
@@ -2,49 +2,6 @@
 error_reporting(0);
 
 ?>
-<!-- Welcome message -->
-<header class="welcome bg-light">
-	<div class="container text-center">
-		<!-- Site title -->
-		<h1><?php echo $title; ?></h1>
-
-		<!-- Site description -->
-		<?php if ($site->description()): ?>
-		<p class="lead"><?php echo $site->description(); ?></p>
-		<?php endif ?>
-
-		<!-- Custom search form if the plugin "search" is enabled -->
-		<?php if (pluginActivated('pluginSearch')): ?>
-		<div class="form-inline d-block">
-			<input id="search-input" class="form-control mr-sm-2" type="search" placeholder="<?php $language->p('Search') ?>" aria-label="Search">
-			<button class="btn btn-outline-primary my-2 my-sm-0" type="button" onClick="searchNow()"><?php $language->p('Search') ?></button>
-			<script>
-				function searchNow() {
-					var searchURL = "<?php echo Theme::siteUrl(); ?>search/";
-					window.open(searchURL + document.getElementById("search-input").value, "_self");
-				}
-				document.getElementById("search-input").onkeypress = function(e) {
-					if (!e) e = window.event;
-					var keyCode = e.keyCode || e.which;
-					if (keyCode == '13') {
-						searchNow();
-						return false;
-					}
-				}
-			</script>
-		</div>
-		<?php endif ?>
-	</div>
-</header>
-
-<?php if (empty($content)): ?>
-	<div class="text-center p-4">
-	<?php $language->p('No pages found') ?>
-	</div>
-<?php endif ?>
-
-<!-- Print all the content -->
-
 
 
 <?php 
@@ -174,7 +131,7 @@ if(isset($_REQ["tx"])){if(stristr($txx,$_REQ["tx"]) == false){continue;}}
 
 				<!-- Page description -->
 				
-				<p class="page-description"><?php echo $valuex; ?><br>
+				<p class="page-description" style="word-break: break-all;"><?php echo $valuex; ?><br>
 				
 				
 
@@ -183,7 +140,7 @@ if(isset($_REQ["tx"])){if(stristr($txx,$_REQ["tx"]) == false){continue;}}
 
 				echo "<br><hr><font size=1>Bi-directional link</font><br>";
 
-				echo "<li style=\"display:block;height:auto;width:900px;padding-top:20px;line-height:40px;font-size:18px;color:#cccccc;\"><p align=left>";
+				echo "<li style=\"display:block;height:auto;width:auto;padding-top:20px;line-height:40px;font-size:18px;color:#cccccc;\"><p align=left>";
 
 				$infob= $kpc->keva_group_filter($gnamespace,"self","",60000);
 				
@@ -230,14 +187,7 @@ if(isset($_REQ["tx"])){if(stristr($txx,$_REQ["tx"]) == false){continue;}}
 				</p>
 			
 
-				<!-- Page content until the pagebreak -->
-				<div>
-				
-				</div>
-
-				
-				<!-- Load Bludit Plugins: Page End -->
-				<?php Theme::plugins('pageEnd'); ?>
+	
 			</div>
 		</div>
 	</div>
