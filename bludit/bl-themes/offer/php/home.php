c835277fc4e44824d74c5c73c8983728d954e10d
kawww <39939137+kawww@users.noreply.github.com>
2022-02-07 11:32:04 +0800

    Add files via upload

--- bludit/bl-themes/offer/php/home.php
+++ bludit/bl-themes/offer/php/home.php
@@ -15,6 +15,8 @@ error_reporting(0);
 			<!-- Post -->
 <?php
 
+
+
 if($pin<>""){
 
 
@@ -36,11 +38,39 @@ echo "</div></div></div></div></div>";
 
 }
 
+//shop
+
+$shopns=$_REQ["asset"];
+
+$checkshop=$kpc->keva_get($shopns,"Congratulations");
 
 
 
 
+if($checkshop['value']!=""){
 
+$checknm=$kpc->keva_get($shopns,"_KEVA_NS_");
+
+echo "<div class=\"card mt-2\">";
+	
+echo "<div class=\"card-body\"><div class=\"card-title\"><br><h3 class=\"text-primary\">".$checknm['value']." SHOP</h3></div>";
+
+echo "<div><div id=\"post-content\">";
+
+
+echo "<div class=\"card-text\">";
+
+echo "This is CHIA OFFER shop, you can use chia wallet to accept offers.";
+
+echo "</div></div></div></div></div>";
+
+
+
+}
+
+echo "<div class=\"card mt-2\">";
+	
+echo "<div class=\"card-body\">";
 
 foreach ($listasset as $k=>$v) 
 
@@ -54,71 +84,13 @@ $value=hex2bin($value);
 
 		if(stristr($key2,"_g") == true){continue;}
 
+		if(stristr($key2,"Congratulations") == true){continue;}
 		if(stristr($key2,"NAMESPACE ADDRESS") == true){continue;}
 		if(stristr($key2,"LOAD ALL BLOCK CONTENTS") == true){continue;}
 
 		
 
-		//check re
 
-		if(strlen($key2) == "64"){
-		
-		
-		
-									$txcount=1;
-									$txloop=$key2;
-
-									
-								
-
-									while($txcount<50) {
-									
-									$txcount++;
-
-									
-
-									$transaction= $kpc->getrawtransaction($txloop,1);
-								
-
-									
-									
-
-									foreach($transaction['vout'] as $vout)
-	   
-									  {
-
-										$op_return = $vout["scriptPubKey"]["asm"]; 
-
-				
-										$arr = explode(' ', $op_return); 
-
-										
-
-										if($arr[0] == 'OP_KEVA_PUT') 
-										{
-											 $cona=$arr[1];
-											 $cons=$arr[2];
-											 $conk=$arr[3];
-
-						
-
-											$txloop=hex2bin($cons);
-
-										
-						
-		
-											if(strlen($txloop)<>64){$key="RE:".$txloop;break;}
-													
-								
-													}
-												
-												}
-											}
-
-										
-		
-		
-		}
 
 			$x_value="<h4>[ ".$key2." ]</h4>";
 
@@ -148,27 +120,31 @@ $value=hex2bin($value);
 			<!-- Load Bludit Plugins: Page Begin -->
 			<?php Theme::plugins('pageBegin'); ?>
 
-			<div class="card mt-2">
-				<div class="card-header"><?php echo $page->category(); ?></div>
-				<img src="" alt="">
-				<div class="card-body">
+		
 					<div class="card-title">
-						<h3 class="text-primary"><?php echo $key; ?></h3>
-					</div>
-					<p class="text-info card-subtitle mb-3"><?php echo "<span ".$rgb."><a href=?theme=".$_REQUEST["theme"]."&asset=".$gnamespace."&gname=".bin2hex($gnamex).">".$gnamex."</a></span>"; ?> -
-					<?php 
-				if(stristr($key,"Congratulations") == true){$key=$_REQUEST["scode"].".txt";}
+						<h4 class="text-primary"><?php 
+				
 				$keyb=bin2hex($key);
 				$valueb=bin2hex($valuex);
-			echo "<a href=?fk=".$keyb."&fv=".$valueb.">[ DOWNLOAD FILE ]</a>"; ?></p>
-					<div class="card-text" style="font-family: 'PingFang SC', 'Noto Sans CJK SC', 'Heiti SC', 'DengXian', 'Microsoft YaHei', Helvetica, Segoe UI, Arial, sans-serif; ">
-						<?php if(stristr($key2,"Congratulations") == true){echo $valuex;} ?>
+				
+				$keyx=str_replace(".offer","",$key);
+				$keyx=str_replace("_x_"," ]</font> ..... ",$keyx);
+
+				$keyx=str_replace("(1)","",$keyx);
+				$keyx=str_replace("(2)","",$keyx);
+				$keyx=str_replace("(3)","",$keyx);
+
+
+			echo "<a href=https://chia.keva.app/offer.php?txid=".$txx.">[ ".$keyx."</a>"; ?></h4>
 					</div>
+				
 					
-				</div>
-			</div>
-			<?php Theme::plugins('pageEnd'); ?>
-			<?php } ?>
+					
+				
+			<?php } 
+			
+			echo "</div></div></div></div></div>";?>
+
         </div>
     </div>
 </div> <!-- END Content Container HOME -->
